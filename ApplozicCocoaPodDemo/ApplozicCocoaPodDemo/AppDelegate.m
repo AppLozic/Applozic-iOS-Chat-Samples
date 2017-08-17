1fa1d49b8c3ee014a93b598bb7d469501cc7e230
adarshmishra <adarsh@applozic.com>
2017-08-17 17:36:15 +0530

    # New pod vesrion update and Appdelegate

--- ApplozicCocoaPodDemo/ApplozicCocoaPodDemo/AppDelegate.m
+++ ApplozicCocoaPodDemo/ApplozicCocoaPodDemo/AppDelegate.m
@@ -1,45 +1,60 @@
 //
 //  AppDelegate.m
-//  ApplozicCocoaPodDemo
+//  ChatApp
 //
-//  Created by Abhishek Thapliyal on 9/8/16.
-//  Copyright © 2016 Abhishek Thapliyal. All rights reserved.
+//  Copyright (c) 2015 AppLozic. All rights reserved.
 //
 
 #import "AppDelegate.h"
-#import <Applozic/Applozic.h>
+#import <Applozic/ALUserDefaultsHandler.h>
+#import <Applozic/ALRegisterUserClientService.h>
+#import <Applozic/ALPushNotificationService.h>
+#import <Applozic/ALUtilityClass.h>
 #import "ChatOptionVC.h"
+#import "Applozic/ALDBHandler.h"
+#import "Applozic/ALMessagesViewController.h"
+#import "Applozic/ALPushAssist.h"
+#import "Applozic/ALMessageService.h"
+#import <UserNotifications/UserNotifications.h>
 
-@interface AppDelegate ()
+
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+
+@interface AppDelegate () <UNUserNotificationCenterDelegate>
+
 
 @end
 
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
     // Override point for customization after application launch.
     
-    
-    
-    /************************************************** APPLOZIC CODE ***************************************************/
-    
+    [self registerForNotification];
     // checks wheather app version is updated/changed then makes server call setting VERSION_CODE
     [ALRegisterUserClientService isAppUpdated];
-    
-    if ([ALUserDefaultsHandler isLoggedIn]) // YOU CAN CHANGE OR ADD YOUR OWN WITH FOR LAUNCH AND COMMENTING IF CODE
-    {
-        [ALPushNotificationService userSync];
-        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-        ChatOptionVC *viewController = (ChatOptionVC *)[storyboard instantiateViewControllerWithIdentifier:@"ChatOptionVC"];
-        [self.window makeKeyAndVisible];
-        [self.window.rootViewController presentViewController:viewController animated:nil completion:nil];
-    }
     
     ALAppLocalNotifications *localNotification = [ALAppLocalNotifications appLocalNotificationHandler];
     [localNotification dataConnectionNotificationHandler];
     
-    NSLog(@"LAUNCH_OPTIONS: %@", launchOptions);
+    if ([ALUserDefaultsHandler isLoggedIn])
+    {
+        [ALPushNotificationService userSync];
+        
+        // Get login screen from storyboard and present it
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        ChatOptionVC *viewController = (ChatOptionVC *)[storyboard instantiateViewControllerWithIdentifier:@"ChatOptionVC"];
+        
+        [self.window makeKeyAndVisible];
+        [self.window.rootViewController presentViewController:viewController
+                                                     animated:nil
+                                                   completion:nil];
+    }
+    
+    NSLog(@"launchOptions: %@", launchOptions);
     
     if (launchOptions != nil)
     {
@@ -48,31 +63,43 @@
         {
             NSLog(@"Launched from push notification: %@", dictionary);
             ALPushNotificationService *pushNotificationService = [[ALPushNotificationService alloc] init];
-            BOOL applozicProcessed = [pushNotificationService processPushNotification:dictionary updateUI:[NSNumber numberWithInt:APP_STATE_INACTIVE]];
-            if (!applozicProcessed)
-            {
-                
+            BOOL applozicProcessed = [pushNotificationService processPushNotification:dictionary
+                                                                             updateUI:[NSNumber numberWithInt:APP_STATE_INACTIVE]];
+            
+            if (!applozicProcessed) {
+                //Note: notification for app
             }
         }
     }
     
-    /*******************************************************************************************************************/
-    
-    
     return YES;
 }
 
+- (void)application:(UIApplication*)application didReceiveRemoteNotification:(NSDictionary*)dictionary {
+    
+    NSLog(@"RECEIVED_NOTIFICATION :: %@", dictionary);
+    ALPushNotificationService *pushNotificationService = [[ALPushNotificationService alloc] init];
+    //    [pushNotificationService processPushNotification:dictionary updateUI:[NSNumber numberWithInt:APP_STATE_INACTIVE]];
+    [pushNotificationService notificationArrivedToApplication:application withDictionary:dictionary];
+}
+
+-(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler{
+    
+    NSLog(@"RECEIVED_NOTIFICATION_WITH_COMPLETION :: %@", userInfo);
+    ALPushNotificationService *pushNotificationService = [[ALPushNotificationService alloc] init];
+    //    [pushNotificationService processPushNotification:userInfo updateUI:[NSNumber numberWithInt:APP_STATE_BACKGROUND]];
+    [pushNotificationService notificationArrivedToApplication:application withDictionary:userInfo];
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    
-    ALRegisterUserClientService *registerUserClientService = [[ALRegisterUserClientService alloc] init];
-    [registerUserClientService disconnect];
     
     NSLog(@"APP_ENTER_IN_BACKGROUND");
     [[NSNotificationCenter defaultCenter] postNotificationName:@"APP_ENTER_IN_BACKGROUND" object:nil];
@@ -81,8 +108,6 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     
-    ALRegisterUserClientService *registerUserClientService = [[ALRegisterUserClientService alloc] init];
-    [registerUserClientService connect];
     [ALPushNotificationService applicationEntersForeground];
     
     NSLog(@"APP_ENTER_IN_FOREGROUND");
@@ -96,32 +121,13 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    // Saves changes in the application's managed object context before the application terminates.
-//    [self saveContext];
     
-      [[ALDBHandler sharedInstance] saveContext];
+    [[ALDBHandler sharedInstance] saveContext];
 }
 
-- (void)application:(UIApplication*)application didReceiveRemoteNotification:(NSDictionary*)dictionary {
-    
-    NSLog(@"Received notification Without Completion :: %@", dictionary);
-    ALPushNotificationService *pushNotificationService = [[ALPushNotificationService alloc] init];
-    [pushNotificationService processPushNotification:dictionary updateUI:[NSNumber numberWithInt:APP_STATE_INACTIVE]];
-}
-
--(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler{
-    
-    NSLog(@"Received notification Completion :: %@", userInfo);
-    ALPushNotificationService *pushNotificationService = [[ALPushNotificationService alloc] init];
-    [pushNotificationService processPushNotification:userInfo updateUI:[NSNumber numberWithInt:APP_STATE_BACKGROUND]];
-    completionHandler(UIBackgroundFetchResultNewData);
-    
-}
-
-- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
-    
-    NSLog(@"TOKEN :: %@", deviceToken);
+    NSLog(@"DEVICE_TOKEN :: %@", deviceToken);
     
     const unsigned *tokenBytes = [deviceToken bytes];
     NSString *hexToken = [NSString stringWithFormat:@"%08x%08x%08x%08x%08x%08x%08x%08x",
@@ -138,17 +144,16 @@
     }
     
     ALRegisterUserClientService *registerUserClientService = [[ALRegisterUserClientService alloc] init];
-    
     [registerUserClientService updateApnDeviceTokenWithCompletion:apnDeviceToken withCompletion:^(ALRegistrationResponse *rResponse, NSError *error) {
         
         if (error)
         {
-            NSLog(@"ERROR updateApnDeviceTokenWithCompletion ::%@",error.description);
+            NSLog(@"REGISTRATION ERROR :: %@",error.description);
             return;
         }
-        NSLog(@"Registration response from server:%@", rResponse);
+        
+        NSLog(@"Registration response from server : %@", rResponse);
     }];
-    
 }
 
 - (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error
@@ -156,83 +161,32 @@
     NSLog(@"Failed to get token, error: %@", error);
 }
 
-#pragma mark - Core Data stack
-
-@synthesize managedObjectContext = _managedObjectContext;
-@synthesize managedObjectModel = _managedObjectModel;
-@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
-
-- (NSURL *)applicationDocumentsDirectory {
-    // The directory the application uses to store the Core Data store file. This code uses a directory named "marvel.ApplozicCocoaPodDemo" in the application's documents directory.
-    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-}
-
-- (NSManagedObjectModel *)managedObjectModel {
-    // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
-    if (_managedObjectModel != nil) {
-        return _managedObjectModel;
+-(void)registerForNotification
+{
+    if(SYSTEM_VERSION_LESS_THAN(@"10.0"))
+    {
+        [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound |    UIUserNotificationTypeAlert | UIUserNotificationTypeBadge) categories:nil]];
+        
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
     }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"ApplozicCocoaPodDemo" withExtension:@"momd"];
-    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-    return _managedObjectModel;
-}
-
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
-    // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it.
-    if (_persistentStoreCoordinator != nil) {
-        return _persistentStoreCoordinator;
-    }
-    
-    // Create the coordinator and store
-    
-    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"ApplozicCocoaPodDemo.sqlite"];
-    NSError *error = nil;
-    NSString *failureReason = @"There was an error creating or loading the application's saved data.";
-    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
-        // Report any error we got.
-        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-        dict[NSLocalizedDescriptionKey] = @"Failed to initialize the application's saved data";
-        dict[NSLocalizedFailureReasonErrorKey] = failureReason;
-        dict[NSUnderlyingErrorKey] = error;
-        error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
-        // Replace this with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
-    }
-    
-    return _persistentStoreCoordinator;
-}
-
-
-- (NSManagedObjectContext *)managedObjectContext {
-    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.)
-    if (_managedObjectContext != nil) {
-        return _managedObjectContext;
-    }
-    
-    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-    if (!coordinator) {
-        return nil;
-    }
-    _managedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
-    [_managedObjectContext setPersistentStoreCoordinator:coordinator];
-    return _managedObjectContext;
-}
-
-#pragma mark - Core Data Saving support
-
-- (void)saveContext {
-    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
-    if (managedObjectContext != nil) {
-        NSError *error = nil;
-        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-            abort();
-        }
+    else
+    {
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+        center.delegate = self;
+        [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error)
+         {
+             if(!error)
+             {
+                 [[UIApplication sharedApplication] registerForRemoteNotifications];  // required to get the app to do anything at all about push notifications
+                 NSLog(@"Push registration success." );
+             }
+             else
+             {
+                 NSLog(@"Push registration FAILED" );
+                 NSLog(@"ERROR: %@ - %@", error.localizedFailureReason, error.localizedDescription );
+                 NSLog(@"SUGGESTIONS: %@ - %@", error.localizedRecoveryOptions, error.localizedRecoverySuggestion );
+             }
+         }];
     }
 }
 
